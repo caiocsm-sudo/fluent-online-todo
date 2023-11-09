226462247f554e532bfa2976d9d13652c389617a
caiocsm-sudo <stampedesw29@gmail.com>
2023-11-08 22:52:00 -0300

    implementing toasty message

--- frontend/src/components/TodoList.tsx
+++ frontend/src/components/TodoList.tsx
@@ -1,11 +1,21 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import { Button, Checkbox, Toaster, ToastTitle, useId, useToastController } from "@fluentui/react-components";
+import {
+  Button,
+  Checkbox,
+  Toast,
+  Toaster,
+  ToastTitle,
+  useId,
+  useToastController,
+  ToastIntent,
+} from "@fluentui/react-components";
+
 import { DeleteRegular, EditRegular } from "@fluentui/react-icons";
 import Progress from "./Progress";
 import styles from "./css/TodoList.module.css";
 
-import axios from 'axios';
+import axios from "axios";
 
 export default function TodoList({
   id,
@@ -18,29 +28,44 @@ export default function TodoList({
   description: string;
   progress: number;
 }) {
-  const toasterId = useId('toaster');
+  const toasterId = useId("toaster");
   const [dashed, setDashed] = useState<boolean>(false);
-  const { dispatchToast } = useToastController();
+  const { dispatchToast } = useToastController(toasterId);
+  const [intent, setIntent] = useState<ToastIntent>("success");
 
   const completeTaskHandler = () => {
     setDashed(true);
-  }
+  };
+
+  const notify = (message: string) =>
+    dispatchToast(
+      <Toast>
+        <ToastTitle>{message}</ToastTitle>
+      </Toast>,
+      { intent }
+    );
 
   const handleDelete = async () => {
-    const result = await axios.delete('http://localhost:8000/' + id);
-    if(result.status === 200) {
-      console.log('yes sir');
+    const result = await axios.delete("http://localhost:8000/" + id);
+    if (result.status === 200) {
+      notify("Task deleted successfully");
+    } else {
+      setIntent("error");
+      notify(`An error occured: ${result.data.detail}`);
     }
     console.log(result);
-  }
+  };
 
   return (
     <li className={styles.list}>
       <div>
-        <Checkbox label={title || "Test Todo"} onClick={completeTaskHandler}></Checkbox>
+        <Checkbox
+          label={title || "Test Todo"}
+          onClick={completeTaskHandler}
+        ></Checkbox>
       </div>
       <div className="description">
-        <span className={styles[`description-text ${dashed ? 'dashed' : ''}`]}>
+        <span className={styles[`description-text ${dashed ? "dashed" : ""}`]}>
           {description || "Test Description"}
         </span>
         <Progress progress={progress || 50} />
