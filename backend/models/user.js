44c49df3a71eb9ec7f1c05825dd9f88d19cd3681
Caio Simões <stampedesw29@gmail.com>
2023-11-13 17:13:06 -0300

    Login functionalities implemented & refactored

--- backend/models/user.js
+++ backend/models/user.js
@@ -8,6 +8,7 @@ class User {
     this.username = username;
     this.user_email = user_email;
     this.password = password;
+    this.id = null;
     this.user = null;
     this.errors = [];
   }
@@ -16,21 +17,40 @@ class User {
     this.checkErrors();
     if (this.errors.length > 0) return;
 
-    const result = await pool.query();
+    const user = this.checkEmail('login');
+
+    if (!user) {
+      this.errors.push("Incorrect email and/or password");
+      return;
+    }
+
+    
+
   }
 
   async register() {
     this.checkErrors();
+    const emailExist = this.checkEmail('register');
+
+    if (emailExist) {
+      this.errors.push("This email is already registered");
+      return;
+    }
+
+    // Everything gone right
+
     if (this.errors.length > 0) return;
 
     this.encryptPassword();
 
-    // const data = await pool.query("INSERT INTO users");
+    this.id = uuidv4();
 
-    const result = await pool.query(
-      `INSERT INTO users (id, username, user_email, password) VALUEs ($1, $2, $3, $4)`,
-      [id, this.username, this.user_email, this.password]
-    );
+    if (this.id) {
+      const result = await pool.query(
+        `INSERT INTO users (id, username, user_email, password) VALUEs ($1, $2, $3, $4)`,
+        [this.id, this.username, this.user_email, this.password]
+      );
+    }
   }
 
   encryptPassword() {
@@ -49,6 +69,18 @@ class User {
     }
     if (this.username.length < 3 && this.username.length > 25) {
       this.errors.push("Your username is not at an accepted length.");
+    }
+  }
+
+  async checkEmail(caller) {
+    const result = await pool.query(
+      "SELECT * FROM users WHERE user_email = $1"[this.user_email]
+    );
+
+    if (caller === 'register') {
+      return result ? true : false;
+    } else {
+      return result;
     }
   }
 }
