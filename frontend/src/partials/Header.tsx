bd3a992b65f76b4e60daae6feadf2c8993e2c616
Caio Simões <stampedesw29@gmail.com>
2023-11-14 21:49:51 -0300

    routing changes

--- frontend/src/partials/Header.tsx
+++ frontend/src/partials/Header.tsx
@@ -1,22 +1,29 @@
 import { Avatar } from "@fluentui/react-components";
 import { DefaultButton, PrimaryButton } from "@fluentui/react";
+import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
 
 // logged in ? avatar : '';
 
 export default function Header() {
-  const loggedIn: boolean = true;
+  const loggedIn: boolean = false;
 
   return (
     <header className={styles.header}>
-      <h1 className={styles.title}>DirectX Todos</h1>
+      <Link to="/">
+        <h1 className={styles.title}>DirectX Todos</h1>
+      </Link>
       <div>
         {loggedIn ? (
           <DefaultButton className={styles.btn}>Log Out</DefaultButton>
         ) : (
           <>
-            <PrimaryButton className={styles.btn}>Log In</PrimaryButton>
-            <DefaultButton className={styles.btn}>Sign Up</DefaultButton>
+            <Link to="/login/login">
+              <PrimaryButton className={styles.btn}>Log In</PrimaryButton>
+            </Link>
+            <Link to="/login/register">
+              <DefaultButton className={styles.btn}>Sign Up</DefaultButton>
+            </Link>
           </>
         )}
         {loggedIn ? (
