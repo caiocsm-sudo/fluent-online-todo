002274a627ffede02c055d906a0add78536c3269
Caio Simões <stampedesw29@gmail.com>
2023-11-17 16:20:38 -0300

    store provider added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,10 +5,15 @@ import "./index.css";
 
 import { FluentProvider, webLightTheme } from "@fluentui/react-components";
 
+import { Provider } from "react-redux";
+import store from "./app/store";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <FluentProvider className="fluent" theme={webLightTheme}>
-      <App />
-    </FluentProvider>
+    <Provider store={store}>
+      <FluentProvider className="fluent" theme={webLightTheme}>
+        <App />
+      </FluentProvider>
+    </Provider>
   </React.StrictMode>
 );
