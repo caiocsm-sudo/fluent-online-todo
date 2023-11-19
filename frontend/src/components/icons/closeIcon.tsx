9ad0cb12d2bf7742afce6e7bd1bc24bca4abcfd4
Caio Simões <stampedesw29@gmail.com>
2023-11-19 18:20:35 -0300

    separated icon

---
+++ frontend/src/components/icons/closeIcon.tsx
@@ -0,0 +1,12 @@
+const CloseIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 2048 2048"
+    className="svg_dd790ee3"
+    focusable="false"
+  >
+    <path d="M1115 1024l690 691-90 90-691-690-691 690-90-90 690-691-690-691 90-90 691 690 691-690 90 90-690 691z"></path>
+  </svg>
+);
+
+export default CloseIcon;
