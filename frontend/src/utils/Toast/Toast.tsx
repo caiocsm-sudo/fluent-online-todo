632399aa75e03ae771d3cf5e50ab09cc30491587
Caio Simões <stampedesw29@gmail.com>
2023-11-24 01:32:02 -0300

    new homepage added + toast

---
+++ frontend/src/utils/Toast/Toast.tsx
@@ -0,0 +1,41 @@
+import { FC } from "react";
+
+import {
+  Toaster,
+  useId,
+  useToastController,
+  Toast,
+  ToastTitle,
+} from "@fluentui/react-components";
+
+type ToastyProps = {
+  message: string;
+}
+
+const Toasty: FC<ToastyProps> = ({ message }: ToastyProps) => {
+  const toasterId = useId("toaster");
+  const { dispatchToast } = useToastController(toasterId);
+
+  const notify = () => {
+    return dispatchToast(
+      <Toast>
+        <ToastTitle>{message}</ToastTitle>
+      </Toast>,
+      { intent: "info" }
+    );
+  };
+
+  return (
+    <>
+      <Toaster
+        toasterId={toasterId}
+        position="top-end"
+        pauseOnHover
+        pauseOnWindowBlur
+        timeout={1000}
+      />
+    </>
+  );
+};
+
+export default Toasty;
